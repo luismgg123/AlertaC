a15ee7a7c9793469e80d9f5a52f30649e1a090a5
luismgg123 <luismgg123@hotmail.com>
2015-11-05 01:25:05 -0500

    ignora el volley :v  el profe hará profundisar la siguiente clase

---
+++ app/src/main/java/yolo/suaj/alertac/EnvioSMS.java
@@ -0,0 +1,23 @@
+package yolo.suaj.alertac;
+
+import android.telephony.SmsManager;
+import android.util.Log;
+
+/**
+ * Created by HP-LM on 31/10/2015.
+ */
+public class EnvioSMS {
+    String telefono, contacto, mensaje;
+    public EnvioSMS(String xtelefono, String xcontacto, String xmensaje){
+        telefono=xtelefono;
+        contacto=xcontacto;
+        mensaje=xmensaje;
+        envio();
+    }
+
+    public void envio(){
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("999956103", null,"hola", null, null);
+        Log.e("Mensaje", "se envio sms");
+    }
+}
--- app/src/main/java/yolo/suaj/alertac/MainActivityFragment.java
+++ app/src/main/java/yolo/suaj/alertac/MainActivityFragment.java
@@ -3,19 +3,27 @@ package yolo.suaj.alertac;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TabHost;
+
+import java.util.ArrayList;
 
 
 public class MainActivityFragment extends Fragment {
     private TabHost tabHost;
 
-    private Button bmaps;
+    private WebView parrafo_1, parrafo_2;
+    private Button bmaps, btnSOS;
 
+    private static final String TAG = "VoiceRecognition";
+
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
     public MainActivityFragment() {
         // Required empty public constructor
     }
@@ -29,6 +37,28 @@ public class MainActivityFragment extends Fragment {
 
         //Codificación
 
+        parrafo_1=(WebView) view.findViewById(R.id.parrafo_1);
+      //  parrafo_2=(WebView) view.findViewById(R.id.parrafo_2);
+
+        String txt_paragraph_1 = "<html><body><div style=text-align:justify;font-size:12px;color:#000000;>"+
+                "<ul>"+
+                "<li>Partida de Nacimiento original</li>"+
+                "<li>Certificado de Estudios Secundarios Original</li>"+
+                "<li>Dos fotografías tamaño carnet</li>"+
+                "<li>Copia de D.N.I</li>"+
+                "<li>Pago por el Derecho de examen de admisión incluido prospecto. El pago se realiza en la Cuenta en soles del Banco de la Nación a la Cta. Cte. N° 0000284475</li>" +
+                "</ul></div></body></html>";
+       parrafo_1.loadData(txt_paragraph_1, "text/html; charset=utf-8","UTF-8");
+
+        btnSOS =(Button) view.findViewById(R.id.btnSOS);
+        btnSOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoiceRecognitionActivity();
+
+
+            }
+        });
         bmaps= (Button) view.findViewById(R.id.bmaps);
         bmaps.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,7 +67,9 @@ public class MainActivityFragment extends Fragment {
                 startActivity(i);
             }
         });
-        //TabHost
+
+
+                //TabHost
         Resources resources = getResources();
         tabHost = (TabHost) view.findViewById(R.id.tabHost);
         tabHost.setup();
@@ -67,6 +99,30 @@ public class MainActivityFragment extends Fragment {
         tabHost.addTab(spec4);
 
         return view;
+    }
+
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify the calling package to identify your application
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Diga:, Auxilio ");
+        startActivityForResult(i, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+    @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == getActivity().RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra
+                    (RecognizerIntent.EXTRA_RESULTS);
+            String[] palabras = matches.get(0).toString().split(" ");
+            palabras[0] = "Auxilio";
+            if (palabras[0].equals("Auxilio")) {
+                EnvioSMS envioSMS = new EnvioSMS("999956103", "999956103", "AUXILIO");
+            }
+        }
     }
 
 
